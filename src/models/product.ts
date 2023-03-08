9dac8e27b2d9734312f5e5a92d2b6c4181b9edcb
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2023-03-08 18:08:01 +0500

    Initial commit

---
+++ src/models/product.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose"
+
+interface IProduct {
+    img: string;
+    name: string;
+    categorieId: any;
+    description: string;
+    price: string;
+    view: number;
+}
+const productSchema = new mongoose.Schema<IProduct>(
+    {
+        img: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        categorieId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "categorie",
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+        view: {
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true
+    },
+);
+export default mongoose.model<IProduct>('product', productSchema);
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from "express";
+import cors from 'cors';
+import mongoose from "mongoose";
+import router from "./routes";
+
+const app = express();
+const mongodb: any = process.env.MONGO_ULR
+mongoose
+    .connect(mongodb)
+    .then(() => console.log("mongodb connected"))
+    .catch((err): any => console.log(err))
+
+app.get('/', (req, res) => {
+    res.send("ok")
+})
+
+app.use(express.json());
+app.use(cors());
+app.use(router)
+
+app.listen('9000', () => {
+    console.log(`Server is running in http://localhost:9000`)
+})
